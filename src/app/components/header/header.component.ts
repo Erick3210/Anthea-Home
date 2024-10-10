cb2baddb15f6eb1eb09ab400c32d4fbba4533836
Erick3210 <efcasba@hotmail.com>
2024-10-10 13:29:17 -0600

    tus cambios

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ChangeDetectionStrategy } from '@angular/core';
+import { AfterViewInit, Component, ChangeDetectionStrategy, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,35 +6,44 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrls: ['./header.component.scss'], // Cambio aquí: styleUrls en lugar de styleUrl
   changeDetection: ChangeDetectionStrategy.Default
-
 })
 export class HeaderComponent implements AfterViewInit {
   menu!: HTMLElement | null;
   menuIcon!: HTMLImageElement | null;
-  show : boolean = false;
+  show: boolean = false;
   iconMenu!: HTMLElement | null;
   iconClose!: HTMLElement | null;
 
   ngAfterViewInit(): void {
   }
 
-  public showMenu(){
+  public showMenu() {
     this.show = !this.show;
     this.iconMenu = document.querySelector('#icon-menu');
     this.iconClose = document.querySelector('#icon-close');
     this.menu = document.querySelector('#menu-mobile');
 
-   if(this.show){
-    this.iconMenu?.classList.add('hiddenIcon')
-    this.iconClose?.classList.remove('hiddenIcon')
-    this.menu?.classList.remove('hiddenIcon')
-   }else{
-    this.iconMenu?.classList.remove('hiddenIcon')
-    this.iconClose?.classList.add('hiddenIcon')
-    this.menu?.classList.add('hiddenIcon')
+    if (this.show) {
+      this.iconMenu?.classList.add('hiddenIcon');
+      this.iconClose?.classList.remove('hiddenIcon');
+      this.menu?.classList.remove('hiddenIcon');
+    } else {
+      this.iconMenu?.classList.remove('hiddenIcon');
+      this.iconClose?.classList.add('hiddenIcon');
+      this.menu?.classList.add('hiddenIcon');
+    }
+  }
 
-   }
+  // Detecta el scroll para cambiar el color del header
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const navbar = document.querySelector('.navbar');
+    if (window.pageYOffset > 50) { // Cambia de color después de 50px de scroll
+      navbar?.classList.add('scrolled');
+    } else {
+      navbar?.classList.remove('scrolled');
+    }
   }
 }
